7052af971fa9c03dc24ab2e4a90ec18a0268e55c
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-05-20 12:02:31 +0200

    ajout icone + divers fix css

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,11 @@ document.getElementsByClassName('fa-bars')[0].addEventListener('click', function
 document.getElementsByClassName('fa-xmark')[0].addEventListener('click', function() {
     document.getElementById('nav-content-mobile').classList.remove('open');
 })
+if (window.innerWidth <= '1024') {
+    document.getElementsByClassName('groupe1')[0].setAttribute('data-aos', 'fade-right');
+    document.getElementsByClassName('groupe2')[0].setAttribute('data-aos', 'fade-right');
+    var boucle = document.getElementsByClassName('containerImg');
+    for (let i = 0; i < boucle.length; i++) {
+        document.getElementsByClassName('containerImg')[i].setAttribute('data-aos', 'fade-right');
+    }
+}
