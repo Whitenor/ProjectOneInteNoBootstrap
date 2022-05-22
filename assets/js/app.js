7f227e7fb5794c486e109b268f7ce7b40b4affc4
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-05-22 14:54:25 +0200

    passage sur des query selector

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,17 +7,17 @@ document.getElementsByClassName('fa-xmark')[0].addEventListener('click', functio
 if (window.innerWidth <= '1024') {
     document.getElementsByClassName('groupe1')[0].setAttribute('data-aos', 'fade-right');
     document.getElementsByClassName('groupe2')[0].setAttribute('data-aos', 'fade-right');
-    var boucle = document.getElementsByClassName('containerImg');
+    var boucle = document.querySelectorAll('.containerImg');
     for (let i = 0; i < boucle.length; i++) {
-        document.getElementsByClassName('containerImg')[i].setAttribute('data-aos', 'fade-right');
+        document.querySelectorAll('.containerImg')[i].setAttribute('data-aos', 'fade-right');
     }
 }
 window.onscroll = () => {
     var scrolling= window.scrollY;
     var viewportHeight = window.innerHeight;
     if(scrolling >= viewportHeight - 100 ){
-      document.getElementsByTagName('nav')[0].classList.add('bgNav')
+      document.querySelector('nav').classList.add('bgNav')
     }else{
-      document.getElementsByTagName('nav')[0].classList.remove('bgNav')
+      document.querySelector('nav').classList.remove('bgNav')
     }
   }
